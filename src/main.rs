12ff1e413fc24e741225bf34bfb8e27a3f2e0632
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-03-23 22:35:06 +0000

    feat: basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 use colored::Colorize;
+use sysinfo::System;
+use whoami::*;
+use std::env;
 
 fn main() {
     let bottom = format!("c({})({})", "\"".red(), "\"".red()).to_string();
     let ascii = vec!["(\\ /)", "( . .)", &bottom];
 
-    for i in ascii {
-        println!("  {}", i);
+    let kernel = System::kernel_version().unwrap_or(String::from("N/A"));
+    let pretty = distro();
+    let wm: &str;
+
+    if cfg!(windows) {
+        wm = "Aero";
+    } else if cfg!(unix) {
+        wm = env!("XDG_CURRENT_DESKTOP");
+    } else {
+        wm = "N/A";
     }
+
+    println!("  {}      {} {}", ascii[0], "Kernel".red(), kernel);
+    println!("  {}     {} {}", ascii[1], "WM".green(), wm);
+    println!("  {}    {} {}", ascii[2], "OS".blue(), pretty);
 }
