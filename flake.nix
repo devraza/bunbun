eed86b104d3675d8962d0769a1cda2a8664b44b3
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-03-24 20:41:55 +0000

    feat: minor release v1.2.0

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
           })
           .buildRustPackage {
             pname = "bunbun";
-            version = "1.1.0";
+            version = "1.2.0";
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
           };
