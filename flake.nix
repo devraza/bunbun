532582911359b7a5c8792e09ec089520d919059a
Muhammad Nauman Raza <devraza@skiff.com>
2024-03-24 14:05:01 +0000

    chore: bump version

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
           })
           .buildRustPackage {
             pname = "bunbun";
-            version = "1.0.0";
+            version = "1.1.0";
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
           };
